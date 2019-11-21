2417a3dc230a116a0714055980faae4a680dc49f
malek0903 <48035855+malek0903@users.noreply.github.com>
2019-11-20 23:00:13 -0800

    minor

---
+++ Pidev/Controllers/TicketController.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using data;
+
+namespace Pidev.Controllers
+{
+    public class TicketController : Controller
+    {
+        // GET: Ticket
+        public ActionResult Index()
+        {
+            HttpClient Client = new HttpClient();
+            Client.BaseAddress = new Uri("");
+            Client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            HttpResponseMessage responce = Client.GetAsync("api/tickets").Result;
+            if(responce.IsSuccessStatusCode)
+            {
+                ViewBag.result = responce.Content.ReadAsAsync<IEnumerable<ticket>>().Result;
+            }else
+            {
+                ViewBag.result = "erruer";
+            }
+            return View();
+        }
+
+        // GET: Ticket/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Ticket/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Ticket/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Ticket/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Ticket/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Ticket/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Ticket/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
