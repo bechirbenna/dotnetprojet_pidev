999fb30618266a88c1336a32b0f1f65dd851ebf5
malek0903 <48035855+malek0903@users.noreply.github.com>
2019-11-23 14:49:47 -0800

    minor

--- Pidev/Controllers/LoginController.cs
+++ Pidev/Controllers/LoginController.cs
@@ -1,89 +1,63 @@
-﻿using System;
+﻿using Microsoft.Owin.Security;
+using Newtonsoft.Json;
+using Pidev.Models;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
+using System.Net.Http;
+using System.Security.Claims;
 using System.Web;
 using System.Web.Mvc;
-
 namespace Pidev.Controllers
 {
     public class LoginController : Controller
     {
         // GET: Login
-        public ActionResult Index()
+        public ActionResult Login(userModel model, string returnUrl)
         {
-            return View();
-        }
-
-        // GET: Login/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-
-        // GET: Login/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Login/Create
-        [HttpPost]
-        public ActionResult Create(FormCollection collection)
-        {
-            try
+            var getTokenUrl = string.Format(Token, ConfigurationManager.AppSettings["localhost:9080/pidev-web/api/athentication"]);
+                 
+            using (HttpClient httpClient = new HttpClient())
             {
-                // TODO: Add insert logic here
+                HttpContent content = new FormUrlEncodedContent(new[]  
+                {
+                    new KeyValuePair<string, string>("username", model.Username),
+                    new KeyValuePair<string, string>("password", model.Password)
+                });
 
-                return RedirectToAction("Index");
+                HttpResponseMessage result = httpClient.PostAsync(getTokenUrl, content).Result;
+
+                string resultContent = result.Content.ReadAsStringAsync().Result;
+
+                var token = JsonConvert.DeserializeObject<Token>(resultContent);
+
+                AuthenticationProperties options = new AuthenticationProperties();
+
+                options.AllowRefresh = true;
+                options.IsPersistent = true;
+                options.ExpiresUtc = DateTime.UtcNow.AddSeconds(int.Parse(token.expires_in));
+
+                var claims = new[]
+                {
+                    new Claim(ClaimTypes.Name, model.Username),
+                    new Claim("AcessToken", string.Format("Bearer {0}", token.access_token)),
+                };
+
+                var identity = new ClaimsIdentity(claims, "ApplicationCookie");
+
+                Request.GetOwinContext().Authentication.SignIn(options, identity);
+
             }
-            catch
-            {
-                return View();
-            }
+
+            return RedirectToAction("Index", "Home");
         }
-
-        // GET: Login/Edit/5
-        public ActionResult Edit(int id)
+        public ActionResult LogOut()
         {
-            return View();
+            Request.GetOwinContext().Authentication.SignOut("ApplicationCookie");
+
+            return RedirectToAction("Login");
         }
-
-        // POST: Login/Edit/5
-        [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Login/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: Login/Delete/5
-        [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
+        
     }
 }
