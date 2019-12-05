70bf14ba58d584fc3a9dbc3d84bf02a67f105e4f
MedOmarWALHA <47657492+MedOmarWALHA@users.noreply.github.com>
2019-12-05 03:39:01 +0100

    Update 2eme consommation

---
+++ Pidev/Controllers/JobController.cs
@@ -0,0 +1,96 @@
+﻿using Pidev.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Pidev.Controllers
+{
+    public class JobController : Controller
+    {
+        // GET: Job
+        public ActionResult Index()
+        {
+            HttpClient Client = new HttpClient();
+            Client.BaseAddress = new Uri("http://localhost:9080/pidev-web/");
+            Client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            HttpResponseMessage responce = Client.GetAsync("api/job").Result;
+            if (responce.IsSuccessStatusCode)
+            {
+                ViewBag.result = responce.Content.ReadAsAsync<IEnumerable<JobModel>>().Result;
+            }
+            else
+            {
+                ViewBag.result = "error";
+            }
+            return View();
+        }
+
+        // GET: Job/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Job/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Job/Create
+        [HttpPost]
+        public ActionResult Create(JobModel job)
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("http://localhost:9080/pidev-web/");
+            //client.PostAsJsonAsync<skill>("api/skill", skill).ContinueWith((postTask) => postTask.Result.EnsureSuccessStatusCode());
+            var response = client.PostAsJsonAsync<JobModel>("api/job", job).Result;
+            return RedirectToAction("Index");
+        }
+
+        // GET: Job/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Job/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, JobModel job)
+        {
+            HttpClient Client = new HttpClient();
+            job.jobId = id;
+            var response1 = Client.PutAsJsonAsync<JobModel>("http://localhost:9080/pidev-web/api/job/", job).Result;
+            return RedirectToAction("index");
+        }
+
+        // GET: Job/Delete/5
+        public ActionResult Delete(int id)
+        {
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = client.DeleteAsync("http://localhost:9080/pidev-web/api/job/" + id.ToString()).Result;
+
+            return RedirectToAction("Index");
+        }
+
+        // POST: Job/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Pidev/Models/JobModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Pidev.Models
+{
+    public class JobModel
+    {
+        public long jobId { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime? jobDate { get; set; }
+        public string jobDesc { get; set; }
+        public string jobName { get; set; }
+    }
+}
